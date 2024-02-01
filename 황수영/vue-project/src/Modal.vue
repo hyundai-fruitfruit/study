ac37f5120f2d1587565dd0dbcc07fd0482df5625
sooyoung <sooyoung.h8@gmail.com>
2024-02-02 06:19:32 +0900

    모달창 컴포넌트화하기

---
+++ 황수영/vue-project/src/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+    
+    <div class="black-bg" v-if="isModalOpen == true"> 
+        <div class="white-bg">
+          <h4>{{ oneroom[isButton].title }}</h4>
+          <p>{{ oneroom[isButton].content }}</p>
+          <p>{{ oneroom[isButton].price }}</p>
+          <button @click="isModalOpen = false">닫기</button>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name : 'ModalComponent',
+    props : {
+      oneroom : Array,
+      isButton : Number,
+      isModalOpen : Boolean
+    }
+}
+</script>
+
+<style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.white-bg {
+  width: 80%; /* Adjust the width as needed */
+  max-width: 600px; /* Set a maximum width if desired */
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
---
+++ 황수영/vue-project/src/assets/apple.js
@@ -0,0 +1,4 @@
+var apple1 = 1
+var apple2 = 10
+
+export {apple1, apple2}
