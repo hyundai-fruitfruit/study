6642ca6d20471acd725960f71fefbea1d998836a
tori <sangeun99@khu.ac.kr>
2024-02-03 12:39:15 +0900

    Part.2 부트스트랩 설치, 적용 및 블로그 글 불러오기

---
+++ 엄상은/vuelog/src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h5>{{ post.title }}</h5>
+    <p>{{ post.date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    post: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
