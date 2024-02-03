410953be0a937f2f52dc4b86da3798fcf7b505e2
tori <sangeun99@khu.ac.kr>
2024-02-03 21:04:00 +0900

    Part 3. 간단한 tab 구현하기

--- 엄상은/vuestagram/src/App.vue
+++ 엄상은/vuestagram/src/App.vue
@@ -17,7 +17,15 @@
       <input type="file" id="file" class="inputfile" />
       <label for="file" class="input-plus">+</label>
     </ul>
- </div>
+  </div>
+
+  <div v-if="step==0">내용0</div>
+  <div v-if="step==1">내용1</div>
+  <div v-if="step==2">내용2</div>
+  <button @click="step=0">버튼0</button>
+  <button @click="step=1">버튼1</button>
+  <button @click="step=2">버튼2</button>
+
 </template>
 
 <script>
@@ -34,6 +42,7 @@ export default {
     return {
       posts: posts,
       moreCount: 0,
+      step: 0
     }
   },
   methods: {
