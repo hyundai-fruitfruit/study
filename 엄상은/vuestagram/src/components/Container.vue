98bf3db1ff824836e86d5cbb93883c91348904b4
tori <sangeun99@khu.ac.kr>
2024-02-03 20:31:53 +0900

    Part 3. 인스타그램 데이터 바인딩, axios 요청

---
+++ 엄상은/vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post v-for="(x, i) in posts" :key="i" :post="x"/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+  components: {
+    Post: Post
+  },
+  props: {
+    posts: Array
+  }
+}
+</script>
+
+<style>
+
+</style>
